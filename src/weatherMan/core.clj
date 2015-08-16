f2ed4e03ad23a4b18f5ea28a2f12f69d5529c3c3
Erik Pearson <emp823@gmail.com>
2015-08-16 14:26:03 -0400

    Practice fns

--- src/weatherMan/core.clj
+++ src/weatherMan/core.clj
@@ -2,10 +2,21 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Testing out functions"
   [& args]
+  (defn addTwo
+    "It adds two to someting"
+    [number]
+    (+ 2 number))
   (if (= 1 1)
-    (do (println (conj [1 2 3] 4))
+    (do (println (map addTwo [1 2 3]))
         "its good")
     (do (println "Not")
-        "wide right")))
+        "wide right"))
+  (defn testDestructure
+    "It takes the first two args and a list of remaining"
+    [[one two & three]]
+    (println (str "One is " one))
+    (println (str "Two is " two))
+    (println (str "Three is " three)))
+  (testDestructure [1 2 3 4 5 6]))
