ba144a528574d388795a6871425c77b84ffc6e64
Erik Pearson <emp823@gmail.com>
2015-09-02 00:35:01 -0400

    hello world

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "A basic weather app"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.4.1"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]]
   :min-lein-version "2.0.0"
   :main weatherMan.core)
