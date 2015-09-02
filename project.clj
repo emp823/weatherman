9f1029280a835824109d1302a3d96f017eadf3a7
Erik Pearson <emp823@gmail.com>
2015-09-01 22:25:37 -0400

    Change dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject weatherMan "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :description "A basic weather app"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
+  :min-lein-version "2.0.0"
   :main weatherMan.core)
